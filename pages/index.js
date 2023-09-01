921bd02241ce1eec09d472287ad19ac7ab18f04f
kamilamin0308 <kamilamin@digitalgravity.ae>
2023-09-02 00:44:57 +0500

    --Modify home page and implement navbar in main app page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='mx-2 bg-red-400'>
-      This is me
+    <div className='container mx-auto px-4'>
+      Home
     </div>
   )
 }
