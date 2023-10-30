461339150a7d9c8bb3eeca394c61822c29b2bcbb
Kamil Amin <kamil.amin@live.co.uk>
2023-10-30 23:58:57 +0500

    Implement add to cart, remove and clear functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,39 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+
+  }, [])
+  const [cart, setCart] = useState([])
+  
+  const [reloadKey, setReloadKey] = useState(1)
+  
+  const addToCart = (item, qty, price) => {
+    let newCart = cart;
+    console.log('Add to cart');
+    for (let index = 0; index < qty; index++) {
+      newCart.push([item, price])
+    }
+    console.log(newCart);
+    setCart(newCart)
+    setReloadKey(Math.random())
+  }
+
+  const removeFromCart = (item, qty) => {
+    let newCart = cart;
+    let index = newCart.indexOf(item)
+    newCart.splice(index)
+    setCart(newCart)
+  }
+
+
+  const clearCart = () => {
+    setCart([])
+  }
   return <>
-    <Navbar />
-    <Component {...pageProps} />
+    <Navbar key={reloadKey} cart={cart} />
+    <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} {...pageProps} />
   </>
 }
