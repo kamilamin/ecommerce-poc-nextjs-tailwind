238a56c33096cac2455c52ca069587849533e6a5
Kamil Amin <kamil.amin@live.co.uk>
2023-11-16 23:56:30 +0500

    Fixed HTML issue tag issues

--- pages/checkout.js
+++ pages/checkout.js
@@ -20,19 +20,19 @@ const checkout = () => {
               </div>
               <div className="p-2 w-1/2">
                 <div className="relative">
-                  <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+                  <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
                   <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
               </div>
               <div className="p-2 w-1/2">
                 <div className="relative">
-                  <label for="phone" className="leading-7 text-sm text-gray-600">Phone</label>
+                  <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone</label>
                   <input type="phone" id="phone" name="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
               </div>
               <div className="p-2 w-full">
                 <div className="relative">
-                  <label for="message" className="leading-7 text-sm text-gray-600">Address</label>
+                  <label htmlFor="message" className="leading-7 text-sm text-gray-600">Address</label>
                   <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                 </div>
               </div>
